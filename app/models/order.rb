a57ea53756994b99582d92443584b8b24e070f9f
chanweiyan <chanweiyan007@gmail.com>
2017-05-17 17:16:20 +0800

    add payment_method function

--- app/models/order.rb
+++ app/models/order.rb
@@ -12,6 +12,8 @@
 #  created_at       :datetime         not null
 #  updated_at       :datetime         not null
 #  token            :string
+#  is_paid          :boolean          default(FALSE)
+#  payment_method   :string
 #
 
 class Order < ApplicationRecord
@@ -27,5 +29,13 @@ class Order < ApplicationRecord
 
   def generate_token
     self.token = SecureRandom.uuid
+  end
+
+  def set_payment_with!(method)
+    self.update_columns(payment_method: method)
+  end
+
+  def pay!
+    self.update_columns(is_paid: true)
   end
 end
